0f9a78167e95d04a258be994718c214ae9e5015d
Aiizbar <andr999andr2006@gmail.com>
2022-02-10 21:44:14 +0500

    first

---
+++ python_web/flask1.py
@@ -0,0 +1,24 @@
+import json
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def passiv():
+    user = 'Миссия Колонизация Марса'
+    return render_template('index.html', username=user, title="Супер-пупер-заголовок")
+
+
+@app.route("/index")
+def index():
+    user = 'И на Марсе будут яблони цвести!'
+    return render_template('index.html', username=user, title="Супер-пупер-заголовок")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
