c75265294905b30a8da4f4f7112e519e853353f0
Aiizbar <andr999andr2006@gmail.com>
2022-02-11 21:01:47 +0500

    theerd

---
+++ python_web/flask3.py
@@ -0,0 +1,43 @@
+import json
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def passiv():
+    user = 'Миссия Колонизация Марса'
+    return render_template('index.html', username=user, title="Супер-пупер-заголовок")
+
+
+@app.route("/index")
+def index():
+    user = 'И на Марсе будут яблони цвести!'
+    return render_template('index.html', username=user, title="Супер-пупер-заголовок")
+
+
+@app.route('/countdown')
+def countdown():
+    countdown_list = [str(x) for x in range(10, 0, -1)]
+    countdown_list.append('Пуск!')
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/p')
+def promotion():
+    countdown_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'По этому, пожалуйста', 'Умри.']
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/image_mars')
+def image():
+    return f'''<img src="{url_for('static', filename='img/MARS.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
